8789139dc475726d6c42fab795e81de67423b291
clo-or <duzyyeicg09@gmail.com>
2025-12-12 22:53:26 +0900

    Update 1_텍스트미디어.py

--- pages/1_텍스트미디어.py
+++ pages/1_텍스트미디어.py
@@ -8,3 +8,12 @@ st.subheader('서브헤더 : st.subheader()')
 st.text('본문 텍스트 : st.text()')
 st.markdown('# 마크다운 : st.markdown()')
 st.caption('캡션(작고 흐린 글씨로 표현됨) : st.caption()')
+
+#: st.write()
+st.write('# 마크다운 H1 : st.write()')
+st.write('## 마크다운 H3 : st.write()')
+st.write('')  # 빈 줄 추가
+
+#: 색상이 있는 텍스트
+st.write(':red[빨간색 텍스트]')
+st.write(':blue[파란색 텍스트]')
