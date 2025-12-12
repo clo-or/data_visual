411d8b1c44960c3ce63d8fdaa05f07ce5736b1bd
clo-or <duzyyeicg09@gmail.com>
2025-12-12 20:09:43 +0900

    Add Streamlit app with sidebar widgets

---
+++ pages/01_abc_텍스트_미디어.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# 타이틀 텍스트 출력
+st.title("Hello, World!")
+
+# 사이드바 설정
+st.sidebar.title('다양한 사이드바 위젯들')
+
+st.sidebar.checkbox('외국인 포함')
+st.sidebar.checkbox('고령인구 포함')
+st.sidebar.divider()  # 구분선
+st.sidebar.radio('데이터 타입', ['전체', '남성', '여성'])
+st.sidebar.slider('나이', 0, 100, (20, 50))
+st.sidebar.selectbox('지역', ['서울', '경기', '인천', '대전', '대구', '부산', '광주'])
