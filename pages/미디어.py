f73678715dbd5387041d186178f6bc727f967e03
clo-or <duzyyeicg09@gmail.com>
2025-12-12 23:09:27 +0900

    Add informational, warning, error, and success messages

--- pages/미디어.py
+++ pages/미디어.py
@@ -11,3 +11,17 @@ st.image("data/heart_mask.png", use_container_width=True)
 
 ## 오디오: st.audio()
 # st.audio("./After_You.mp3", format="audio/mpeg", loop=True)
+
+# 콜아웃
+
+## 정보: st.info()
+st.info('This is a purely informational message', icon="i")
+
+### 경고: st.warning()
+st.warning('This is a warning message', icon="A")
+
+## 에러: st.error()
+st.error('This is an error message', icon="")
+
+#### 성공: st.success()
+st.success('This is a success message', icon="")
