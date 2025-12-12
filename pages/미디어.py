5470e7d515744c38d9c7e6478ab701d6359f9bf5
clo-or <duzyyeicg09@gmail.com>
2025-12-12 23:08:01 +0900

    Fix image path for heart_mask.png

--- pages/미디어.py
+++ pages/미디어.py
@@ -7,8 +7,7 @@ st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # YouTube 링크
 
 ## 이미지: st.image()
 # pages 폴더 기준 상위 data 폴더의 파일 경로
-st.image("../data/heart_mask.png", use_container_width=True)
-
+st.image("data/heart_mask.png", use_container_width=True)
 
 ## 오디오: st.audio()
 # st.audio("./After_You.mp3", format="audio/mpeg", loop=True)
