0af02e00435d8540adc8e2118df23066805e5e1f
clo-or <duzyyeicg09@gmail.com>
2025-12-12 23:05:01 +0900

    Update 1_텍스트미디어.py

--- pages/1_텍스트미디어.py
+++ pages/1_텍스트미디어.py
@@ -66,16 +66,4 @@ def hello_world():
     print("Hello, World!")
 """, language='python')
 
-# 미디어 삽입
-
-## 이미지: st.image()
-# pages 폴더 기준 상위 data 폴더의 파일 경로
-st.image("../data/heart_mask.png", caption="파이썬 로고", use_container_width=True)
-
-
-## 오디오: st.audio()
-# st.audio("./After_You.mp3", format="audio/mpeg", loop=True)
-
-## 동영상: st.video()
-st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # YouTube 링크
 
