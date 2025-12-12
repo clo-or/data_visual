6be4deb288d2e3ab1163ac16fcd3d1358a8d9765
clo-or <duzyyeicg09@gmail.com>
2025-12-12 16:54:12 +0900

    Delete a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import streamlit as st
-
-st.title("Streamlit 설치 확인")
-st.write("Streamlit이 정상적으로 실행되고 있습니다!")
-
-number = st.number_input("숫자를 입력해보세요", 0, 100)
-st.write("입력한 값:", number)
