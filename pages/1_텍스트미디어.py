d6558706e18fcbc3fadf80617be9fbfaf62f9044
clo-or <duzyyeicg09@gmail.com>
2025-12-12 22:51:46 +0900

    Create 1_텍스트미디어.py

---
+++ pages/1_텍스트미디어.py
@@ -0,0 +1,8 @@
+'## : 일반 텍스트'
+st.title('제목 : st.title()')
+
+st.header('헤더 : st.header()')
+st.subheader('서브헤더 : st.subheader()')
+st.text('본문 텍스트 : st.text()')
+st.markdown('# 마크다운 : st.markdown()')
+st.caption('캡션(작고 흐린 글씨로 표현됨) : st.caption()')
