03dd9237ce72321c78aa4e73f0f1b048d8386a5e
clo-or <duzyyeicg09@gmail.com>
2025-12-12 23:05:54 +0900

    Refactor video insertion and clean up code
    Removed duplicate video insertion code and added comment.

--- pages/미디어.py
+++ pages/미디어.py
@@ -1,5 +1,8 @@
 # 미디어 삽입
 
+## 동영상: st.video()
+st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # YouTube 링크
+
 ## 이미지: st.image()
 # pages 폴더 기준 상위 data 폴더의 파일 경로
 st.image("../data/heart_mask.png", use_container_width=True)
@@ -7,6 +10,3 @@ st.image("../data/heart_mask.png", use_container_width=True)
 
 ## 오디오: st.audio()
 # st.audio("./After_You.mp3", format="audio/mpeg", loop=True)
-
-## 동영상: st.video()
-st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # YouTube 링크
